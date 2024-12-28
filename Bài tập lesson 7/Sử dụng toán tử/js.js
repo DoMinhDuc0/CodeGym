5feb9107e255b972b5f358db4620e06e6d5958ab
Duc <ducdo19052006@gmail.com>
2024-12-28 21:19:23 +0700

    first commit

---
+++ Bài tập lesson 7/Sử dụng toán tử/js.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter your input width");
+inputHeight = prompt("Enter your input height");
+console.log(typeof inputWidth);
+console.log(typeof inputHeight);
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
